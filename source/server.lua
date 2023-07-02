62082a0ca96a6a78c70bd689e7925fec2c047395
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-07-02 11:25:42 -0700

    Update server.lua

--- source/server.lua
+++ source/server.lua
@@ -1,6 +1,7 @@
 startup = function()
 print([[
-    Kezi GunStorePlus Started | Verison ^41.0.0
+    Zaps GunStorePlus Started | Verison ^41.0.0
+        Support? https://discord.gg/qP3rCzMCMH
 ]])
 
 end
